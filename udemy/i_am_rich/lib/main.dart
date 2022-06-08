9533260b71aa14464b8fff42817ed0c8de082c42
xandecoelho5 <xandecoelho5@hotmail.com>
2022-06-07 22:33:45 -0300

    Added Project i_am_rich

---
+++ udemy/i_am_rich/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image.asset('images/diamond.png'),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
