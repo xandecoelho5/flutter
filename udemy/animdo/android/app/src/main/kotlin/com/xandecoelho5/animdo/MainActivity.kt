9e7f37ef75cb9551f29e64a390f8ee24193a049e
xandecoelho5 <xandecoelho5@hotmail.com>
2022-06-21 18:29:48 -0300

    Added project Animdo

---
+++ udemy/animdo/android/app/src/main/kotlin/com/xandecoelho5/animdo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.xandecoelho5.animdo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ udemy/animdo/lib/pages/home_page.dart
@@ -0,0 +1,101 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  double _buttonRadius = 100;
+
+  final Tween<double> _backgroundScale = Tween(begin: 0.0, end: 1.0);
+
+  AnimationController? _starIconAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _starIconAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    );
+    _starIconAnimationController!.repeat();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          _pageBackground(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _circularAnimationButton(),
+              _starIcon(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _pageBackground() {
+    return TweenAnimationBuilder(
+      tween: _backgroundScale,
+      curve: Curves.easeInOutCubicEmphasized,
+      duration: const Duration(seconds: 1),
+      builder: (context, double scale, child) {
+        return Transform.scale(scale: scale, child: child);
+      },
+      child: Container(color: Colors.blue),
+    );
+  }
+
+  Widget _circularAnimationButton() {
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            _buttonRadius += _buttonRadius == 200 ? -100 : 100;
+          });
+        },
+        child: AnimatedContainer(
+          duration: const Duration(seconds: 2),
+          curve: Curves.bounceInOut,
+          height: _buttonRadius,
+          width: _buttonRadius,
+          decoration: BoxDecoration(
+            color: Colors.purple,
+            borderRadius: BorderRadius.circular(_buttonRadius),
+          ),
+          child: const Center(
+            child: Text(
+              'Basic',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _starIcon() {
+    return AnimatedBuilder(
+      animation: _starIconAnimationController!.view,
+      builder: (context, child) {
+        return Transform.rotate(
+          angle: _starIconAnimationController!.value * 2 * pi,
+          child: child,
+        );
+      },
+      child: const Icon(Icons.star, size: 100, color: Colors.white),
+    );
+  }
+}
